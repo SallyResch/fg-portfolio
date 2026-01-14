2bce032a389ffa4ae65634758d7c26da5e659886
SallyResch <sallyresch27@gmail.com>
2026-01-14 10:28:41 +0100

    added grid display to projects page för the project-cards. added pictures

--- scripts/script.js
+++ scripts/script.js
@@ -16,11 +16,18 @@ const fetchRepos = async () => {
             .filter(repo => !repo.fork)
             .forEach(repo => {
                 projectsContainer.innerHTML += `
-          <article class="project">
+          <article class="project-card">
+            <img
+            src="./images/ac2eea7ff7c76aff36c2e7a090f7018f.png"
+            alt="picture of girl coding"
+            width="1024"
+            hight="1024"
+            class="coding-picture"
+            />
             <h3>${repo.name}</h3>
             <p>${repo.description || "No description available."}</p>
             <a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">
-              GitHub
+              GitHub Link
             </a>
           </article>
         `;
