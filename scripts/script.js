e1d0aabc7369d26aaf89c49ff1361086b541cd49
SallyResch <sallyresch27@gmail.com>
2026-01-29 10:02:52 +0100

    start from scratch with project page

--- scripts/script.js
+++ scripts/script.js
@@ -2,58 +2,10 @@ const projectsContainer = document.querySelector(".projects");
 
 const reposApi =
   "https://api.github.com/users/SallyResch/repos";
+
 const repoLanguagesApi =
   "https://api.github.com/repos/SallyResch/{repo}/languages";
 
-
-const fetchRepoLanguages = async (repoName) => {
-  try {
-    const response = await fetch(
-      repoLanguagesApi.replace("{repo}", repoName)
-    );
-
-    if (!response.ok) {
-      throw new Error("Could not fetch languages");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Language fetch error:", error);
-    return {};
-  }
-};
-
-const renderProject = (repo, languages) => {
-  const languageList = Object.keys(languages);
-
-  projectsContainer.innerHTML += `
-    <article class="project-card">
-            <iframe
-            src=${repo.homepage}
-            frameborder="1"
-            scrolling="no"
-          ></iframe>
-      <h3>${repo.name}</h3>
-      <p>Description:</p>
-      <p>${repo.description || "No description available."}</p>
-      <div class="languages">
-        <h4>Languages</h4>
-        ${languageList.length
-      ? languageList
-        .map(lang => `<span class="language-tag">${lang}</span>`)
-        .join("")
-      : "<p>No languages found</p>"
-    }
-      </div>
-      <a href="${repo.homepage}" target="_blank" rel="noopener noreferrer">
-        Website
-      </a>
-      <a href="${repo.html_url}" target="_blank" rel="noopener noreferrer">
-        GitHub Repo
-      </a>
-    </article>
-  `;
-};
 const fetchRepos = async () => {
   try {
     const response = await fetch(reposApi);
@@ -75,5 +27,26 @@ const fetchRepos = async () => {
     projectsContainer.innerHTML = "<p>Could not load projects.</p>";
   }
 };
-
 fetchRepos();
+
+
+const fetchRepoLanguages = async (repoName) => {
+  try {
+    const response = await fetch(
+      repoLanguagesApi.replace("{repo}", repoName)
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch languages");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Language fetch error:", error);
+    return {};
+  }
+};
+
+const renderProject = (repo, languages) => {
+
+};
